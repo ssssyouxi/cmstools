1b527ee52962c8458f24bef0e3b1a9895c3ae767
ssssyouxi <361490334@qq.com>
2017-07-03 13:10:51 +0800

    修改生成的数据库文件的名称

--- rename.php
+++ rename.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-type: application/json');
 if ($_POST["rename"]="db") {
-	if (rename("SpiderResult.db3","SpiderResult.db")) {
+	if (rename("SpiderResult.db3",date("Y-m-d-H-i-s").".db")) {
 		echo json_encode(["err"=>null,"msg"=>"自动修改后缀名成功"]);
 	}else{
 		echo json_encode(["err"=>"后缀名修改失败，请自行将数据库后缀名修改为.db"]);
